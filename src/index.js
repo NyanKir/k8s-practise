f032962a205b5fbc2f4035248667e37b133e31cd
Sytnyanskiy Kirill <nyan.kirill@gmail.com>
2024-01-12 16:37:41 +0300

    feat:init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const {createServer} = require("http");
+const host = 'localhost';
+const port = 8000;
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My server!");
+};
+
+const server = createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
